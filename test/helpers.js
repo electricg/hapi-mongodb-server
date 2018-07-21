f7c38e26d2b88f894e11833e70857a386da26650
Giulia Alfonsi <electric.g@gmail.com>
2018-07-21 11:46:18 -0700

    Probably fixed the mockgoose problems

--- test/helpers.js
+++ test/helpers.js
@@ -11,18 +11,60 @@ const server = require('../src/lib/server');
 const mongoose = new Mongoose();
 const mockgoose = new Mockgoose(mongoose);
 
+// Doing this because otherwise Mockgoose connects to the real MongoDB and not to the one in the RAM
+// https://github.com/Mockgoose/Mockgoose/issues/22#issuecomment-328848967
+Mockgoose.prototype.prepareStorage = function(port) {
+  const _this = this;
+  return new Promise(resolve => {
+    Promise.all([_this.getTempDBPath(), _this.getOpenPort()]).then(
+      promiseValues => {
+        const dbPath = promiseValues[0];
+        const openPort = port;
+        const storageEngine = _this.getMemoryStorageName();
+        const mongodArgs = [
+          '--port',
+          openPort,
+          '--storageEngine',
+          storageEngine,
+          '--dbpath',
+          dbPath,
+        ];
+        _this.mongodHelper.mongoBin.commandArguments = mongodArgs;
+        const mockConnection = () => {
+          _this.mockConnectCalls(_this.getMockConnectionString(openPort));
+          resolve();
+        };
+        _this.mongodHelper
+          .run()
+          .then(mockConnection)
+          .catch(mockConnection);
+      }
+    );
+  });
+};
+
 module.exports.config = config;
 module.exports.db = db;
 
 module.exports.baseUrl = `http://${config.get('host')}:${config.get('port')}`;
 
+let getConfigStub;
 module.exports.dbStart = async () => {
-  await mockgoose.prepareStorage();
+  // Doing this because otherwise Mockgoose connects to the real MongoDB and not to the one in the RAM
+  const PORT = 27016;
+  getConfigStub = sinon.stub(config, 'get');
+  getConfigStub
+    .withArgs('mongodbUrl')
+    .returns(`mongodb://localhost:${PORT}/test`);
+  await mockgoose.prepareStorage(PORT);
+
   await db.connect();
 };
 
 module.exports.dbStop = async () => {
+  getConfigStub.restore();
   await db.close();
+  // Probably not needed anymore, but keeping it just in case
   // https://github.com/Mockgoose/Mockgoose/issues/33#issuecomment-321835220
   // mockgoose.mongodHelper.mongoBin.childProcess.kill('SIGKILL');
 };
@@ -60,5 +102,6 @@ after(async () => {
   utilsLogStub.restore();
 });
 
+// Probably not needed anymore, but keeping it just in case
 // https://github.com/nodejs/node/issues/9523#issuecomment-259303079
 // process.on('unhandledRejection', r => console.log(r));
