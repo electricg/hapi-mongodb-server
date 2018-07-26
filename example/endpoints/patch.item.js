32335b5fbc3b17a8b319f49d4b1cb601a18c005d
Giulia Alfonsi <electric.g@gmail.com>
2018-07-26 03:00:39 -0700

    Added PATCH /item

--- example/endpoints/patch.item.js
+++ example/endpoints/patch.item.js
@@ -3,7 +3,23 @@ const collection = require('../collections/items');
 
 module.exports.handler = (request, h) => {
   return new Promise(resolve => {
-    const { id } = request.params;
-    resolve({ id });
+    const { params, payload } = request;
+    const { id } = params;
+    collection
+      .patch({ id, body: payload })
+      .then(doc => {
+        resolve(doc);
+      })
+      .catch(err => {
+        if (err === 404) {
+          return resolve(
+            h.response(utils.formatError('Item not found', err)).code(404)
+          );
+        }
+
+        resolve(
+          h.response(utils.formatError('Internal MongoDB error', err)).code(500)
+        );
+      });
   });
 };
