06d0e113613cb8cdf0b6807936f62ecf57a1701f
Giulia Alfonsi <electric.g@gmail.com>
2018-07-26 02:51:38 -0700

    Added DELETE /item

--- example/endpoints/delete.item.js
+++ example/endpoints/delete.item.js
@@ -4,6 +4,21 @@ const collection = require('../collections/items');
 module.exports.handler = (request, h) => {
   return new Promise(resolve => {
     const { id } = request.params;
-    resolve({ id });
+    collection
+      .remove(id)
+      .then(doc => {
+        resolve(doc);
+      })
+      .catch(err => {
+        if (err === 404) {
+          return resolve(
+            h.response(utils.formatError('Item not found', err)).code(404)
+          );
+        }
+
+        resolve(
+          h.response(utils.formatError('Internal MongoDB error', err)).code(500)
+        );
+      });
   });
 };
